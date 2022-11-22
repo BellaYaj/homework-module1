ec8b54726b4a67bafb85453293d141aaa821f09b
week1-day1 <i.yang.355@westcliff.edu>
2022-11-21 17:04:37 -0800

    css commit 12

--- eternal.js
+++ eternal.js
@@ -23,3 +23,28 @@
             setInterval(currentTime, 1000); //setting timer
         //Initial run of time data function
             currentTime()
+
+<script>
+const container = document.querySelector(".container");
+const preview = document.querySelector(".prev");
+const active = document.querySelectorAll(".thumb");
+ 
+container.addEventListener("click", function (e) {
+  if (e.target.className == "thumb") {
+    preview.src = e.target.src;
+    preview.classList.add("effect");
+ 
+    setTimeout(function () {
+      preview.classList.remove("effect");
+    }, 100);
+ 
+    active.forEach(function (thumb) {
+      if (thumb.classList.contains("active")) {
+        thumb.classList.remove("active");
+      }
+    });
+ 
+    e.target.classList.add("active");
+  }
+});
+</script>
